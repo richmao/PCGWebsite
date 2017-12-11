7dc1ad0acf423e44d978d341d2f8265c75476e6d
weblings <weblings.lings@gmail.com>
2017-12-10 16:51:12 -0800

    Buttons affected by css. All base right column grammars in

--- randomizer.js
+++ randomizer.js
@@ -5,9 +5,12 @@ var buttonText = ["Academics","Finances","Personal Information","Class Button","
 
 let htmlRules = {
   'origin':'#sections# #sections# #sections# #sections# #sections#',
-  'sections': ['#classButton#', '#holds#'],
-  'classButton': ['<div id=\"classButton\"><div class=\"border\"><h5 class=\"button\">SEARCH FOR CLASSES</h5></div></div>'],
-  'holds':['<div id=\"holds\"><table class=\"schedule\"><tr class=\"notSchedule\"><th class=\"box\"><img src=\"arrow.png\">Holds</th><th class=\"box\"></th></tr><tr class=\"boxHold\"><td class=\"box\">No Holds</td><td class=\"box\"></td></tr></table></div>']
+  'sections': ['#classButton#', '#holds#', '#toDo#','#enrollment#','#records#'],
+  'classButton': ['<div id=\"classButton\"><div class=\"border\"><h5 class=\"button\">SEARCH FOR CLASSES</h5></div></div><div class=\"vertSpacer\"></div>'],
+  'holds':['<div id=\"holds\"><table class=\"schedule\"><tr class=\"notSchedule\"><th class=\"box\"><img src=\"arrow.png\">Holds</th><th class=\"box\"></th></tr><tr class=\"boxHold\"><td class=\"box\">No Holds</td><td class=\"box\"></td></tr></table></div><div class=\"vertSpacer\"></div>'],
+  'toDo':['<div id=\"toDo\"><table class=\"schedule\"><tr class=\"notSchedule\"><th class=\"box\"><img src=\"arrow.png\">To Do List</th><th class=\"box\"></th></tr><tr class=\"box\"><td class=\"boxLink\">Authorization to Release Info</td><td class=\"box\"></td></tr><tr class=\"box\"><td class=\"boxSpacer\"></td><td class=\"boxSpacer\"></td></tr><tr class=\"box\"><td class=\"boxSpacer\"></td><td class=\"boxEnder\">more<img src=\"arrow3.png\"></td></tr></table></div><div class=\"vertSpacer\"></div>'],
+  'enrollment':['<div id=\"enrollment\"><table class=\"schedule\"><tr class=\"notSchedule\"><th class=\"box\"><img src=\"arrow.png\">Enrollment Dates</th><th class=\"box\"></th></tr><tr class=\"box\"><td class=\"boxDescriptor\">Enrollment Appointment</td><td class=\"box\"></td></tr><tr class=\"box\"><td class=\"box\">You may begin enrolling for the 2018 Winter Quarter Regular Academic Session session on November 27, 2017</td>    <td class=\"boxSpacer\"></td></tr><tr class=\"box\"><td class=\"boxSpacer\"></td><td class=\"boxEnder\">details<img src=\"arrow3.png\"></td></tr></table></div><div class=\"vertSpacer\"></div>'],
+  'records':['<div id=\"records\"><table class=\"schedule\"><tr class=\"notSchedule\"><th class=\"box\"><img src=\"arrow.png\">Student Records and Enrollment</th><th class=\"box\"></th></tr><tr class=\"box\"><td class=\"boxLink\">California Voter Registration</td><td class=\"box\"></td></tr></table></div<div class=\"vertSpacer\"></div>']
 }
 
 function htmlRedo(){
@@ -30,6 +33,7 @@ cssGrammar = function () {
         "div.links", "div.vertSpacer", "div#dropdown", "div.border", "img.arrow2", "table.schedule", "table.notSchedule",
         "table.box", "tr.schedule", "tr.box", "tr.boxHolder", "th.schedule", "th.notSchedule", "th.box", "td.schedule",
         "td.vertSpacer", "td.descriptor", "td.link", "td.box", "td.boxLink", "td.boxSpacer", "td.boxEnder", "td.boxDescriptor",
+        "button#reRollB","button#academicsB","button#financesB","button#personalInfoB","button#classButtonB","button#holdsB","button#toDoB","button#enrollmentB","button#recordsB"
     ];
     tags.forEach(function (tag) {
         var toChange = cssGrammar.flatten("#origin#").split(" ");
@@ -48,9 +52,9 @@ cssGrammar = function () {
 
 window.onload = function () {
     document.getElementById("reRollB").addEventListener("click", function () {
-        cssGrammar();
         htmlRedo();
         resetButtons();
+        cssGrammar();
     })
     resetButtons();
 }
